4b584c006cdb5d3874545388e4240f062f99e6f6
Christian Weiske <cweiske@cweiske.de>
2020-11-26 17:27:18 +0100

    Allow multiple instances of braus
    This fixes opening braus several times with different URLs
    
    Resolves: https://github.com/properlypurple/braus/issues/20

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ class Application(Gtk.Application):
         super().__init__(
             *args,
             application_id='com.properlypurple.braus',
-            flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+            flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE | Gio.ApplicationFlags.NON_UNIQUE,
             **kwargs
         )
 
